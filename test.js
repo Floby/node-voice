cce86ab1f17843e0d4563ab84a6646b977a35cf5
Florent Jaby <florent.jaby@gmail.com>
2010-09-24 12:33:45 +0200

    cleaned up a bit

--- test.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-
-var sys = require('sys');
-var zip = require('./gzip');
-
-zip.gzip(process.openStdin(), process.stdout);
--- test2.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-
-var sys = require('sys');
-var zip = require('./gzip');
-
-zip.gunzip(process.openStdin(), process.stdout);
